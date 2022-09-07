24893f6a1bfb8b89b4473eb128bfedafd9edf0d9
Chivano295 <crijsdam@hotmail.com>
2022-09-07 11:04:25 +0200

    ubvug

---
+++ My project/Assets/Scripts/Killarrow.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Killarrow : MonoBehaviour
+{
+   public void KillArrow()
+    {
+        Destroy(gameObject);
+    }
+}
