a6370e367c00180c39b36696c32d3c1fc26ca88d
Chivano295 <crijsdam@hotmail.com>
2022-10-07 10:56:51 +0200

    randowm spawn

--- My project/Assets/OurScripts/RandomSpawn.cs
+++ My project/Assets/OurScripts/RandomSpawn.cs
@@ -7,17 +7,38 @@ public class RandomSpawn : MonoBehaviour
     public Transform[] spawnpoints;
     [SerializeField]private GameObject gun;
     [SerializeField] private GameObject[] items;
-
+    [SerializeField] private int itemAmount = 5;
+    [SerializeField] private GameObject[] currentItems;
+ 
     public void Start()
     {
         SpawnObjects();
     }
 
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            CallSpawn();
+        }
+    }
     public void SpawnObjects()
     {
-        int random = Random.RandomRange(0, 3);
+        for (int i = 0; i < itemAmount; i++)
+        {
+            currentItems[i] = Instantiate(items[Random.Range(0, 10)], spawnpoints[i]);
+        }
+            
 
-        
-        Instantiate(items[Random.Range(0 ,5)], spawnpoints[random]);
+    }
+
+    public void CallSpawn()
+    {     
+         GameObject.Destroy(currentItems[0]);
+        GameObject.Destroy(currentItems[1]);
+        GameObject.Destroy(currentItems[2]);
+        GameObject.Destroy(currentItems[3]);
+        GameObject.Destroy(currentItems[4]);
+        SpawnObjects();
     }
 }
