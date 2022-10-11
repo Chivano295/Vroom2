32c7a1174e8cae854fc1b9e227b110671ec6fc9b
JukiAzter <200561@student.glu.nl>
2022-10-11 16:24:57 +0200

    Start menu Update

--- My project/Assets/OurScripts/Johmi/StartScreenClicker.cs
+++ My project/Assets/OurScripts/Johmi/StartScreenClicker.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class StartScreenClicker : MonoBehaviour
 {
     [SerializeField] private GameObject _raycastObject;
+    float startTime;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
             RaycastHit objectHit;
             Vector3 fwd = _raycastObject.transform.TransformDirection(Vector3.forward);
             if (Physics.Raycast(_raycastObject.transform.position, fwd, out objectHit, 100))
             {
-                if (objectHit.transform.GetComponent<VRUIButtonClick>() != null)
+            if (objectHit.transform.GetComponent<VRUIButtonClick>() != null)
+            { 
+                startTime++; 
+            }
+            if (startTime >= 200 && objectHit.transform.GetComponent<VRUIButtonClick>() != null)
                     objectHit.transform.GetComponent<VRUIButtonClick>().VRUnityUIButtonInvoke();
             }
-        }
+        //}
     }
 }
